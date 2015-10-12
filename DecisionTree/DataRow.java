251d63b2a3a7ad3304063963f31c922058f80cb2
omegaga <omegaleedh@gmail.com>
2015-10-12 13:52:51 -0400

    fix bug in hw2

--- DecisionTree/DataRow.java
+++ DecisionTree/DataRow.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,6 +8,18 @@ public class DataRow {
     this.features = features;
     this.label = label;
   }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (getClass() != obj.getClass())
+      return false;
+    DataRow other = (DataRow) obj;
+    for (int i = 0; i < features.size(); ++i)
+      if (this.features.get(i).equals(other.features.get(i)))
+        return false;
+    return true;
+  }
+
   public List<Double> features;
   public int label;
 }
--- DecisionTree/TreeNode.java
+++ DecisionTree/TreeNode.java
@@ -1,4 +1,3 @@
-import javax.xml.crypto.Data;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -15,6 +14,7 @@ public class TreeNode {
   public Double entropy;
   public Integer selectedFeature;
   public Double selectedThreshold;
+  public Double maxIG;
 
   public TreeNode(Set<Integer> remainFeatures, List<DataRow> dataMatrix) {
     this.remainFeatures = remainFeatures;
@@ -27,8 +27,16 @@ public class TreeNode {
    */
   private void initValues() {
     int[] labelCount = new int[2];
+    Boolean flag = true;
+    DataRow lastRow = null;
     for (DataRow row: dataMatrix) {
       labelCount[row.label]++;
+      if (lastRow != null) {
+        if (row != lastRow)
+          flag = false;
+      } else {
+        lastRow = row;
+      }
     }
 
     int sampleCount = dataMatrix.size();
@@ -47,12 +55,18 @@ public class TreeNode {
 
     // Update label
     this.label = -1;
-    if (this.remainFeatures.size() == 0) {
-      this.label = (labelCount[1] > labelCount[0]) ? 1 : 0;
-    } else if (labelCount[0] == 0) {
+    if (labelCount[0] == 0) {
       this.label = 1;
     } else if (labelCount[1] == 0) {
       this.label = 0;
+    }
+    if (flag) {
+      if (labelCount[1] != labelCount[0]) {
+        this.label = labelCount[0] > labelCount[1] ? 0 : 1;
+      } else {
+        this.label = Math.random() < 0.5 ? 0 : 1;
+      }
+
     }
   }
 
@@ -62,7 +76,7 @@ public class TreeNode {
   public void construct() {
     if (label != -1)
       return;
-    findThreshold();
+    maxIG = findThreshold();
     splitTree();
     leftChild.construct();
     rightChild.construct();
@@ -72,89 +86,91 @@ public class TreeNode {
    * Find the feature that maximizes information gain. For each feature, scan
    * through all possible threshold to find the largest information gain.
    */
-  public void findThreshold() {
-    double maxScore = Double.MIN_VALUE;
+  public double findThreshold() {
+    double maxScore = Double.NEGATIVE_INFINITY;
+    int sampleCount = dataMatrix.size();
+
+    int[] totalLabelCount = new int[2];
+    for (DataRow row : dataMatrix)
+      totalLabelCount[row.label]++;
 
     for (int feature: remainFeatures) {
-      int[] totalLabelCount = new int[2];
       int[] currentLabelCount = new int[2];
-      int sampleCount = dataMatrix.size();
 
       List<FeatureLabelTuple> featureList = new ArrayList<>();
-
       // Pre-processing
-      for (int i = 0; i < sampleCount; ++i) {
-        DataRow row = dataMatrix.get(i);
-        FeatureLabelTuple tuple = new FeatureLabelTuple(row.features.get(feature),
-            row.label);
+      for (DataRow row : dataMatrix) {
+        FeatureLabelTuple tuple =
+            new FeatureLabelTuple(row.features.get(feature), row.label);
         featureList.add(tuple);
-        totalLabelCount[row.label]++;
       }
 
       Collections.sort(featureList);
 
-      currentLabelCount[dataMatrix.get(0).label]++;
       for (int threshold = 1; threshold < sampleCount; ++threshold) {
-        if (featureList.get(threshold).label !=
-            featureList.get(threshold - 1).label) {
-          // Calculate information gain
-          double score = 0;
+        currentLabelCount[featureList.get(threshold - 1).label]++;
+        if (featureList.get(threshold).feature.equals(
+            featureList.get(threshold - 1).feature))
+          continue;
+        // Calculate information gain
+        double score = 0;
 
-          // Score below threshold
-          double belowScore = 0;
-          for (int i = 0; i < 2; ++i) {
-            double p = ((double) currentLabelCount[i]) / threshold;
-            if (p == 0)
-              belowScore += 0;
-            else
-              belowScore += -p * (Math.log(p) / Math.log(2));
-          }
-          score += belowScore * ((double) threshold) / sampleCount;
-
-          // Score above threshold
-          double aboveScore = 0;
-          for (int i = 0; i < 2; ++i) {
-            double p = ((double) (totalLabelCount[i] - currentLabelCount[i]))
-                / (sampleCount - threshold);
-            if (p == 0)
-              aboveScore += 0;
-            else
-              aboveScore += -p * (Math.log(p) / Math.log(2));
-          }
-          score += aboveScore *
-              ((double) sampleCount - threshold) / sampleCount;
-
-          score = entropy - score;
-          if (score > maxScore) {
-            maxScore = score;
-            selectedFeature = feature;
-            selectedThreshold = featureList.get(threshold).feature;
-          }
+        // Score below threshold
+        double belowScore = 0;
+        for (int i = 0; i < 2; ++i) {
+          double p = ((double) currentLabelCount[i]) / threshold;
+          if (p == 0)
+            belowScore += 0;
+          else
+            belowScore += -p * (Math.log(p) / Math.log(2));
         }
-        currentLabelCount[dataMatrix.get(0).label]++;
+        score += belowScore * ((double) threshold) / sampleCount;
+
+        // Score above threshold
+        double aboveScore = 0;
+        for (int i = 0; i < 2; ++i) {
+          double p = ((double) (totalLabelCount[i] - currentLabelCount[i]))
+              / (sampleCount - threshold);
+          if (p == 0)
+            aboveScore += 0;
+          else
+            aboveScore += -p * (Math.log(p) / Math.log(2));
+        }
+        score += aboveScore * ((double) sampleCount - threshold) / sampleCount;
+
+        score = entropy - score;
+        if (score > maxScore) {
+          maxScore = score;
+          selectedFeature = feature;
+          selectedThreshold = featureList.get(threshold).feature;
+        }
       }
     }
+    return maxScore;
   }
 
   /**
    * Split the tree with selected feature and threshold
    */
-  public void splitTree() {
+  public Boolean splitTree() {
     List<DataRow> leftData =
         this.dataMatrix.stream()
             .filter(x -> x.features.get(selectedFeature) < selectedThreshold)
             .collect(Collectors.toList());
-    Set<Integer> leftRemainFeatures = new HashSet<>(this.remainFeatures);
-    leftRemainFeatures.remove(selectedFeature);
-    this.leftChild = new TreeNode(leftRemainFeatures, leftData);
 
     List<DataRow> rightData =
         this.dataMatrix.stream()
             .filter(x -> x.features.get(selectedFeature) >= selectedThreshold)
             .collect(Collectors.toList());
+
+    Set<Integer> leftRemainFeatures = new HashSet<>(this.remainFeatures);
+
+    this.leftChild = new TreeNode(leftRemainFeatures, leftData);
     Set<Integer> rightRemainFeatures = new HashSet<>(this.remainFeatures);
-    rightRemainFeatures.remove(selectedFeature);
+
     this.rightChild = new TreeNode(rightRemainFeatures, rightData);
+
+    return true;
   }
 
   /**
